833bd8bd7813cb79981f998f9fd366e7bf7a70b9
alunoalgoritmoseprogramacao <alunoalgoritmos@gmail.com>
2026-04-16 00:24:42 +0000

    aula do dia 15/04

--- Multiplic.java
+++ Multiplic.java
@@ -3,7 +3,7 @@ void main() {
     int n1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
     int n2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
     
-    int soma = n1 + n2;
+    int multiplicacao = n1 * n2;
     
-    IO.println("A soma é: " + soma);
+    IO.println("A multiplicacao é: " + multiplicacao);
 }
