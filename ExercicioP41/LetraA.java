6305af33b2b558ea5d7cbacb509bd576b84462ee
Isamara Costa <jcisamara@gmail.com>
2026-04-27 17:40:00 +0000

    estudo

---
+++ ExercicioP41/LetraA.java
@@ -0,0 +1,18 @@
+//4-a) Ler dois valores numéricos inteiros e apresentar 
+// o resultado da diferença do maior pelo menor valor.
+
+void main(){
+
+    float a = Float.parseFloat(IO.readln("Informe o primeiro número A: "));
+    float b = Float.parseFloat(IO.readln("Informe o segundo número B: "));
+
+    float resultado;
+
+     if (a>b) {
+       resultado =a-b;
+       IO.println("A difereça de A-B é: " + resultado); 
+    } else {
+        resultado = b-a;
+        IO.println("A diferença de B-A é: " + resultado);
+    }
+}
---
+++ ExercicioP41/LetraB.java
@@ -0,0 +1,15 @@
+//Efetuar a leitura de um valor inteiro positivo ou negativo 
+// e apresentar o número lido como sendo um valor positivo, 
+// ou seja, o programa deverá apresentar o módulo de um número fornecido. 
+// Lembre-se de verificar se o número fornecido é menor que zero; sendo, multiplique-o por -1.
+
+void main() {
+
+    int Num1 = Integer.parseInt(IO.readln("Insira um número: "));
+
+    if (Num1<1) {
+        
+    } else {
+        
+    }
+}
