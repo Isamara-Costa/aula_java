e96fecb675c44b5394eceec712004d8d00158a77
Isamara Costa <jcisamara@gmail.com>
2026-05-06 23:14:58 +0000

    aula06/05

---
+++ 04_vetores/Vecto.java
@@ -0,0 +1,13 @@
+
+void main(){
+
+    var num=new int[5];
+    
+    for(int cont=0; cont<5; cont++)
+        num [cont] = Integer.parseInt(IO.readln("Digite o " + (cont+1)+ "o numero: "));
+    
+    IO.println("Estes foram os numeros digitados:");
+
+    for(int cont=0;cont<5;cont++)
+        IO.println("Posição " + (cont) + " numero "+ num[cont]);
+}
